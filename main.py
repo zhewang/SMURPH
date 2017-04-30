6fba6b76698383995469629fc8dfff285815cad4
Zhe Wang <zwang.contact@gmail.com>
2017-04-30 00:14:12 -0700

    plot different classes

--- main.py
+++ main.py
@@ -31,20 +31,32 @@ def calculateKernelforDB():
     with open('./DB/list.txt', 'r') as f:
         for line in f.readlines():
             pc_list.append(loadPoints('./DB/'+line.rstrip('\n')))
-    k = smurph.kernelMP(pc_list, [0.1], 20, 350, 1)
+    k = smurph.kernelMP(pc_list, [0.1], 10, 100, 1)
     np.savetxt('kernel.txt', k)
 
-def plot2D(kernel):
+def plot2D(kernel, classes, markers, colors):
     U, s, V = np.linalg.svd(kernel, full_matrices=True)
     result = U.dot(np.diag(np.sqrt(s)))
     x = result[:,0]
     y = result[:,1]
-    plt.scatter(x, y)
+
+    for xp, yp, c in zip(x, y, classes):
+        plt.scatter(xp, yp, marker=markers[c], c=colors[c])
     plt.show()
 
 
 if __name__ == '__main__':
-    calculateKernelforDB()
+    # calculateKernelforDB()
 
     kernel = np.loadtxt('kernel.txt')
-    plot2D(kernel)
+    # 1: long bottle, 2: bowl, 3: knife; 4: small can; 5:mug, 6:glass
+    # 7: pan with handle
+    classes = np.array([
+        1,1,2,2,2, 3,3,3,3,4, 4,4,1,3,6,
+        3,7,6,6,3, 3,6,5,5,5, 7,7,7,7,3,
+        3,3,3,3,3, 3,7,3,3,1, 6
+    ])
+    markers = {1:'^', 2:'h', 3:'8', 4:'*', 5:'D', 6:'o', 7:'s'}
+    colors = { 1:'#e41a1c',2:'#377eb8',3:'#4daf4a',4:'#984ea3',
+              5:'#ff7f00',6:'#ffff33',7:'#a65628'}
+    plot2D(kernel, classes, markers, colors)
