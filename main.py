759e4a42f5d70060ca29ff96eec890e520b448fa
Zhe Wang <zwang.contact@gmail.com>
2017-04-30 18:51:08 -0700

    generate point cloud list with different number of holes

--- main.py
+++ main.py
@@ -1,14 +1,80 @@
 import random
 
 import scipy.io as sio
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
+from scipy.spatial.distance import euclidean
 from sklearn.decomposition import KernelPCA
 
 import smurph
 import linear
+
+
+def plotPoints(points):
+    parray = np.array(points)
+    plt.figure(figsize=(6,6))
+    plt.scatter(parray[:,0], parray[:,1], s = 3)
+    plt.axis('off')
+    plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05)
+
+    plt.show()
+
+def dataset_diffHoles():
+
+    # generate a grid 100*100
+    grid = []
+    for i in range(41):
+        for j in range(41):
+            if euclidean((i,j), (20,20)) < 20:
+                grid.append((i, j))
+
+    radius = [2,3,4,5]
+    points_list = []
+    classes = []
+
+    # bites
+    center = (20, 0)
+    for r in radius:
+        points = []
+        for p in grid:
+            if euclidean(p, center) > r:
+                points.append(p)
+        points_list.append(points)
+        classes.append(1)
+
+    # 1 hole
+    center = (20, 20)
+    for r in radius:
+        points = []
+        for p in grid:
+            if euclidean(p, center) > r:
+                points.append(p)
+        points_list.append(points)
+        classes.append(2)
+
+    # 2 holes
+    centers = [(10, 20), (30, 20)]
+    for r in radius:
+        points = []
+        for p in grid:
+            if euclidean(p, centers[0]) > r and euclidean(p, centers[1]) > r:
+                points.append(p)
+        points_list.append(points)
+        classes.append(3)
+
+    # 3 holes
+    centers = [(12, 12), (20, 30), (28, 12)]
+    for r in radius:
+        points = []
+        for p in grid:
+            if euclidean(p, centers[0]) > r and euclidean(p, centers[1]) > r and euclidean(p, centers[2]) > r:
+                points.append(p)
+        points_list.append(points)
+        classes.append(4)
+
+    return points_list, classes
+
 
 # load FirstMM_object_data [M. Neumann 2013]
 def loadPoints(fpath):
@@ -85,4 +151,11 @@ def exp_DB_linear():
 
 if __name__ == '__main__':
     # exp_DB()
-    exp_DB_linear()
+    # exp_DB_linear()
+    points_list, classes = dataset_diffHoles()
+    k = smurph.kernelMP(points_list, [40,20,10], 20, 100, 1)
+    markers = {1:'^', 2:'h', 3:'8', 4:'*', 5:'D', 6:'o', 7:'s'}
+    colors = { 1:'#e41a1c',2:'#377eb8',3:'#4daf4a',4:'#984ea3',
+              5:'#ff7f00',6:'#ffff33',7:'#a65628'}
+    plot2D(kernel, classes, markers, colors)
+
