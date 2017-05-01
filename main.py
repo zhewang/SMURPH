1fe2997e437ba6598a555804e0f0b456ebc20a99
Zhe Wang <zwang.contact@gmail.com>
2017-05-01 10:35:41 -0700

    generate multiscale dataset

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 import scipy.io as sio
 import matplotlib.pyplot as plt
@@ -15,7 +16,7 @@ def plotPoints(points):
     parray = np.array(points)
     plt.figure(figsize=(6,6))
     plt.scatter(parray[:,0], parray[:,1], s = 3)
-    plt.axis('off')
+    # plt.axis('off')
     plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05)
 
     plt.show()
@@ -76,7 +77,64 @@ def dataset_diffHoles():
     return points_list, classes
 
 def dataset_multiscale():
-    pass
+    points_list = []
+    classes = []
+    labels = []
+
+    points = []
+    step = 0.3
+    for i in np.arange(0, 41, step):
+        for j in np.arange(0, 41, step):
+            if euclidean((i,j), (20,20)) < 20 and euclidean((i,j), (20,20)) > 15:
+                points.append((i, j))
+
+    points_list.append(points)
+    classes.append(1)
+    labels.append('o,s,1')
+
+    # generate centers
+    center_count = 32
+    centers = []
+    for t in np.arange(0, 2*np.pi, 2*np.pi/center_count):
+        centers.append((math.cos(t)*17.5+20, math.sin(t)*17.5+20))
+
+    points = []
+    for p in points_list[0]:
+        valid = True
+        for c in centers:
+            if euclidean(p,c) < 1:
+                valid = False
+                break
+        if valid is True:
+            points.append(p)
+
+    points_list.append(points)
+    classes.append(2)
+    labels.append('o,h,1')
+
+    # double the above two
+    doubled_list = []
+    for points in points_list:
+        new_points = []
+        for p in points:
+            new_points.append(p)
+            new_points.append( (p[0]+40, p[1]))
+        doubled_list.append(new_points)
+        classes.append(3)
+
+
+    # scale the above four
+    scaled_list = []
+    for points in points_list+doubled_list:
+        for alpha in [0.6, 0.2]:
+            scaled = []
+            for p in points:
+                scaled.append( [x*alpha for x in p] )
+            scaled_list.append(scaled)
+            classes.append(4)
+
+    return points_list+scaled_list+doubled_list, classes
+
 
 ################################################################################
 
@@ -163,8 +221,21 @@ def exp_multiholes():
               5:'#ff7f00',6:'#ffff33',7:'#a65628'}
     plot2D(k, classes, markers, colors)
 
+def exp_multiscale():
+    points_list, classes = dataset_multiscale()
+
+    k = smurph.kernelMP(points_list, [50], 5, 300, 1)
+    np.savetxt('kernel.txt', k)
+
+    k = np.loadtxt('kernel.txt')
+    markers = {1:'^', 2:'h', 3:'8', 4:'*', 5:'D', 6:'o', 7:'s'}
+    colors = { 1:'#e41a1c',2:'#377eb8',3:'#4daf4a',4:'#984ea3',
+              5:'#ff7f00',6:'#ffff33',7:'#a65628'}
+    plot2D(k, classes, markers, colors)
+
 
 if __name__ == '__main__':
     # exp_DB()
     # exp_DB_linear()
-    exp_multiholes()
+    # exp_multiholes()
+    exp_multiscale()
